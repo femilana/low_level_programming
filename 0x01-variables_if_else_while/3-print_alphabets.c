c9e75fe945bdbd2ad71e010c0c71f09854fba914
femilana <femilanah@gmail.com>
2023-06-19 16:51:41 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point of code
+ * Return: 0
+ */
+
+int main(void)
+{
+	char let;
+
+	char yes;
+
+	let = 'a';
+	yes = 'A';
+	while
+	(let <= 'z') { 
+			putchar (let);
+			let++;
+		}
+	while 
+	(yes <= 'Z') { 
+			putchar (yes);
+		}
+	putchar('\n');
+	return (0);
