042205f7df7bb68554c0df162230d0f82db8009d
femilana <femilanah@gmail.com>
2023-06-20 10:26:02 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int base_16;
 
-	for (base_16 = 0; base_16 < 15; base_16++)
+	for (base_16 = 0; base_16 <= 15; base_16++)
 	{
 		putchar(base_16 < 10 ? base_16 + '0' : base_16 + 'a' - 10);
 	}
