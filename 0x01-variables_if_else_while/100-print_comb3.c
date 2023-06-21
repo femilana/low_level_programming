ae253c704bbb57483b5428a31070af862f1ea2f3
femilana <femilanah@gmail.com>
2023-06-21 08:46:02 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num_1, num_2;
+
+	for (num_1 = 0; num_1 <= 8; num_1++)
+	{
+	for (num_2 = 0; num_2 <= 9; num_1++)
+	{
+		putchar(num_1 + '0');
+		putchar(num_2 + '0');
+
+		if (num_1 != 8 || num_2 != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
