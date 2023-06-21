42bdd408ce98c8c9de363b91f414cc58ed9ffeab
femilana <femilanah@gmail.com>
2023-06-21 08:00:31 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,19 +6,20 @@
 
 int main(void)
 {
-	int num;
+	int num = 0;
 
-	for (num = 0; num <= 9; num++)
+	while (num <= 9)
+		num++;
 	{
 		putchar(num + '0');
 
-		if (num < 9)
+		if (num != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
