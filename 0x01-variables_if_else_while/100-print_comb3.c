456b33486ef72288df84738f957226ce5d194d6a
femilana <femilanah@gmail.com>
2023-06-21 08:54:23 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (num_1 = 0; num_1 <= 8; num_1++)
 	{
-	for (num_2 = num_1 ; num_2 <= 9; num_2++)
+	for (num_2 = num_1 + 1 ; num_2 <= 9; num_2++)
 	{
 		putchar(num_1 + '0');
 		putchar(num_2 + '0');
