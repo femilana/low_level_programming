bf9c792cd3f9e62d46a17cdaf4944104c0d44125
femilana <femilanah@gmail.com>
2023-06-22 20:06:07 -0700

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Check if the num is +ve , -ve or 0
+ * Return:
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+	{
+		printf("%d is %s\n", i, "negative");
+	}
+	else if (i > 0)
+	{
+		printf("%d is %s\n", i, "positive");
+	}
+	else
+	{
+		printf("%d is %s\n", i, "zero");
+	}
+	return;
+}
