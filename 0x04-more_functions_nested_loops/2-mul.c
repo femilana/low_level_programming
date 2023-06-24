c9fa7911adc16cf3c44cba1be837178e52334ccc
femilana <femilanah@gmail.com>
2023-06-23 20:36:31 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies the int 'a' and 'b'
+ * @a: The first int to be multiplied
+ * @b: The secound int to be multiplied
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+
+	return (result);
+}
