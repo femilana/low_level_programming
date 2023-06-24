cfd6f269c9d10c73ef81e430c2c497e13fb30f19
femilana <femilanah@gmail.com>
2023-06-23 21:46:39 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - diagonal line
+ * @n: input
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
