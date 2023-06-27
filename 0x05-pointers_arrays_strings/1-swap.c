407604ed3af9e7e07d3ea95ff7f7c70b8d783430
femilana <femilanah@gmail.com>
2023-06-26 19:38:40 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * void swap_int - swap two parameter
+ * @a: swap and store address of parameter 'b'
+ * @b: swap and store address of parameter 'a'
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *b;
+	*b = *a;
+	*a = swap;
+}
