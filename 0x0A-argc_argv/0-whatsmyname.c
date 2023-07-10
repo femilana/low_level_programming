5ddbec134af62862eba7d0e5c4bc1f307a524cc2
femilana <femilanah@gmail.com>
2023-07-09 17:04:26 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argv: argument array
+ * @argc: argument counter
+ * Return: argc
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("the program name is %s\n", argv[0]);
+	return (argc);
+
+}
