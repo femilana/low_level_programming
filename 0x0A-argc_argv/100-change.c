dea423640ff56babe273c1282d961a0b12d8ff1e
femilana <femilanah@gmail.com>
2023-07-11 07:05:58 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: num of arc
+ * @argv: array of argv
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num, a, result;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (a = 0; a < 5 && num >= 0; a++)
+	{
+		while (num >= coins[a])
+		{
+			result++;
+			num -= coins[a];
+		}
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
